e3cbe25cad742b330f9b11f31043206d242384f4
dmitry <dmitrsafonov@gmail.com>
2022-09-06 18:52:59 +0300

    пятое задание

--- ex_5.py
+++ ex_5.py
@@ -12,4 +12,3 @@ L = math.sqrt((x2-x1)**2+(y2-y1)**2)
 print (round (L,2 )) 
 
 
-#Подробнее – на otvet.ya.guru – https: // otvet.ya.guru/questions/5078927-python-napishite-programmu-kotoraya-vvodit-koordinaty-dvuh-tochek.html
