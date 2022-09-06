77aa399a7ecccf20bef6d8c493e798bb6aa12c15
dmitry <dmitrsafonov@gmail.com>
2022-09-06 17:03:12 +0300

    второе задание

--- ex_2.py
+++ ex_2.py
@@ -1,23 +1,9 @@
-#2. Напишите программу для. проверки истинности утверждения 
-# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат#
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Введите значение {xyz[i]}: "))
-    return a
+# 2. Напишите программу для. проверки истинности утверждения 
+#¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
+print('x\ty\tz')
+for x in range(2):
+    for y in range(2):
+       for z in range(2):
+            res = not(x or y or z) == (not(x) and not(y and not(z)))
+            print(f'{x} {y} {z} {res}')
 
-
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
-
-
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print(f"Утверждение истинно")
-else:
-    print(f"Утверждение ложно")
